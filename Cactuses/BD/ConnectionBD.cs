0656ae79851061a047f44ec39a9ac88e06a2b4de
Yulia Karelova <j.karelowa@gmail.com>
2024-12-23 10:43:10 +0300

    Добавьте файлы проекта.

---
+++ Cactuses/BD/ConnectionBD.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cactuses.BD
+{
+    internal class ConnectionBD
+    {
+        public static KaktusiEntities kaktusi = new KaktusiEntities();
+
+    }
+}
